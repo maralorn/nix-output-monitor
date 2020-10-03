83c43fa314fcfe7a27f0a8b1fb183a912c20d006
Malte Brandy <malte.brandy@maralorn.de>
2020-10-03 16:33:19 +0200

    Remove warnings from test

--- test/Golden.hs
+++ test/Golden.hs
@@ -2,8 +2,6 @@ module Main where
 import           Prelude                        ( )
 import           Relude
 import           Test.HUnit
-import           Data.Attoparsec.Text.Lazy
-import           Data.Set
 import           Parser
 import           Data.Text.Lazy.IO             as LTextIO
 import           System.Process
@@ -25,14 +23,16 @@ main = do
     [ "Golden 1" ~: do
         -- First we ensure that the necessary derivations exist in the store
         callProcess "nix-build" ["test/golden1.nix", "--no-out-link"]
-        now      <- getCurrentTime
-        log      <- LTextIO.readFile "test/golden1.log"
+        now           <- getCurrentTime
+        log           <- LTextIO.readFile "test/golden1.log"
         endState <- processText parser (initalState now) updateState Nothing log
-        state    <- LTextIO.readFile "test/golden1.state"
-        let fixTime       = read "2020-10-03 12:54:17.029638514 UTC"
-            expectedState = read $ toString state
+        expectedState <- read . toString <$> LTextIO.readFile
+          "test/golden1.state"
         assertEqual "State matches"
                     expectedState
                     (endState { startTime = fixTime, currentTime = fixTime })
     ]
   if (errors counts + failures counts == 0) then exitSuccess else exitFailure
+
+fixTime :: UTCTime
+fixTime = read "2020-10-03 12:54:17.029638514 UTC"
