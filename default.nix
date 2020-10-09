87670ae79bae897c465992dc82562e3ba69bc852
Malte Brandy <malte.brandy@maralorn.de>
2020-10-09 22:15:34 +0200

    Rename Test

--- default.nix
+++ default.nix
@@ -1,15 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
-
-# Generate cabal2nix.nix with cabal2nix.
-
 pkgs.haskell.lib.overrideCabal
 (pkgs.haskellPackages.callCabal2nix "nix-output-monitor" ./. { }) {
   # Can't ran the golden-tests with nix, because they call nix
   testTarget = "unit-tests";
-  src = fetchFromGitHub {
-    owner = "maralorn";
-    repo = "nix-output-monitor";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
-    rev = "a0e0b09";
-  };
 }
