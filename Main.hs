1e384f72f9318649caca35efd1b09c4c620421e9
Joe Hermaszewski <git@monoid.al>
2020-11-09 10:58:00 +0800

    Add basic help text

--- Main.hs
+++ Main.hs
@@ -8,8 +8,32 @@ import           Print
 import           Update
 import           IO
 import           Data.Time
+import           Data.Text.IO                   ( hPutStrLn )
+import           System.Environment             ( getArgs )
 
 main :: IO ()
 main = do
-  now      <- getCurrentTime
+  args <- getArgs
+  unless (null args) $ do
+    hPutStrLn stderr helpText
+    -- It's not a mistake if the user requests the help text, otherwise tell
+    -- them off with a non-zero exit code.
+    if any ((== "-h") <||> (== "--help")) args then exitSuccess else exitFailure
+  now <- getCurrentTime
   processStream parser (initalState now) updateState stateToText
+
+helpText :: Text
+helpText = unlines
+  [ "Usage: nix-build |& nom"
+  , ""
+  , "Run any nix command (nixos-rebuild,nix-build,home-manager switch,"
+  , "not nix build.) and pipe stderr and stdout into nom."
+  , ""
+  , "Don‘t forget to redirect stderr, too. That's what the & does."
+  , ""
+  , "Please see the readme for more details:"
+  , "https://github.com/maralorn/nix-output-monitor"
+  ]
+
+(<||>) :: Applicative f => f Bool -> f Bool -> f Bool
+(<||>) = liftA2 (||)
