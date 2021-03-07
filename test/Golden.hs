e792716e70aa6557d2546200fe2cf79528b9e313
Malte Brandy <malte.brandy@maralorn.de>
2021-03-07 20:17:07 +0100

    Fix golden1 test by renaming nix-log and commiting it

--- test/Golden.hs
+++ test/Golden.hs
@@ -21,7 +21,7 @@ main = do
             -- First we ensure that the necessary derivations exist in the store
             pathForced <- doesFileExist "/nix/store/5gki8h3p9g63mp9q6xy9z7hlq1haqq6j-remote-build"
             unless pathForced $ callProcess "nix-build" ["test/golden1.nix", "--no-out-link"]
-            log <- LTextIO.readFile "test/golden1.log"
+            log <- LTextIO.readFile "test/golden1.nix-build-output"
             firstState <- initalState
             stateVar <- newTVarIO firstState
             endState <- processText parser stateVar updateState Nothing log
