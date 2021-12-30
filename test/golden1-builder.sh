745d7479c63c65125522a4624eefc8f78bf5db8b
Malte Brandy <malte.brandy@maralorn.de>
2021-12-30 05:31:58 +0100

    Don‘t loop in the test build

--- test/golden1-builder.sh
+++ test/golden1-builder.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-echo "Hanging if local-build"
-if [[ "$name" == "remote-build" ]];
-then
-while true
-do
-echo "loop" > /dev/null
-done
-fi
 echo "test" > $out
+if [[ "$name" == "local-build2" ]];
+then
+   echo "local-build2 is coded to fail in the builder"
+   exit 42
+fi
 echo "Finishing build"
