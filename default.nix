22f2df5f2b4eafdaedba36fb56f35202866ab58e
Malte Brandy <malte.brandy@maralorn.de>
2022-01-06 00:02:15 +0100

    Fix test

--- default.nix
+++ default.nix
@@ -1,9 +1,2 @@
-{ sources ? import nix/sources.nix, pkgs ? import sources.nixpkgs {} }:
-let
-  golden-test = import ./test/golden1.nix;
-in
-pkgs.haskell.lib.overrideCabal
-  (pkgs.haskellPackages.callCabal2nix "nix-output-monitor" ./. {})
-  {
-    preCheck = "# ${golden-test.local} ${golden-test.remote} ${golden-test.local.drvPath} ${golden-test.remote.drvPath}";
-  }
+{ sources ? import nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
+pkgs.haskell.lib.overrideCabal (pkgs.haskellPackages.callCabal2nix "nix-output-monitor" ./. { }) { testTarget = "unitTests"; }
