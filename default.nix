6fcaa629a2c0a8f682e1c35d238fc86952e49838
Malte Brandy <malte.brandy@maralorn.de>
2022-01-06 00:08:24 +0100

    Fix default.nix

--- default.nix
+++ default.nix
@@ -1,2 +1,4 @@
 { sources ? import nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
-pkgs.haskell.lib.overrideCabal (pkgs.haskellPackages.callCabal2nix "nix-output-monitor" ./. { }) { testTarget = "unitTests"; }
+pkgs.haskell.lib.overrideCabal
+  (pkgs.haskellPackages.callCabal2nix "nix-output-monitor" ./. { })
+{ testTarget = "unit-tests"; }
