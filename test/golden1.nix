c19ad63bfd56c7966610a40c200e5eafc11bd0e7
Malte Brandy <malte.brandy@maralorn.de>
2022-03-08 02:15:06 +0100

    Reformat

--- test/golden1.nix
+++ test/golden1.nix
@@ -1,13 +1,15 @@
-{ seed, system ? builtins.currentSystem }:
-let
-  mkBuild = name: deps: derivation {
-    inherit name deps seed system;
-    builder = ./golden1-builder.sh;
-  };
-in
-rec {
-  build1 = mkBuild "build1" [ ];
-  build2 = mkBuild "build2" [ build1 ];
-  build3 = mkBuild "build3" [ build1 build2 ];
-  build4 = mkBuild "build4" [ build2 build3 ];
+{
+  seed,
+  system ? builtins.currentSystem,
+}: let
+  mkBuild = name: deps:
+    derivation {
+      inherit name deps seed system;
+      builder = ./golden1-builder.sh;
+    };
+in rec {
+  build1 = mkBuild "build1" [];
+  build2 = mkBuild "build2" [build1];
+  build3 = mkBuild "build3" [build1 build2];
+  build4 = mkBuild "build4" [build2 build3];
 }
