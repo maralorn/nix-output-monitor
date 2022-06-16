c376f7aca0bdc6f318991494b66d269a9e5febde
Malte Brandy <malte.brandy@maralorn.de>
2022-06-17 01:58:22 +0200

    Hide cursor during running nom

--- exe/Main.hs
+++ exe/Main.hs
@@ -21,6 +21,7 @@ import NOM.State.CacheId.Map qualified as CMap
 import NOM.Update (detectLocalFinishedBuilds, maintainState, updateState)
 import NOM.Update.Monad (UpdateMonad)
 import NOM.Util (addPrintCache, (<|>>), (<||>))
+import System.Console.ANSI qualified as Terminal
 
 main :: IO ()
 main = do
@@ -34,9 +35,14 @@ main = do
       -- It's not a mistake if the user requests the help text, otherwise tell
       -- them off with a non-zero exit code.
       if any ((== "-h") <||> (== "--help")) xs then exitSuccess else exitFailure
+
+  Terminal.hideCursor
+
   firstState <- initalState
   let firstCompoundState = (Nothing, firstState, stateToText firstState)
   (_, finalState, _) <- interact parser compoundStateUpdater (_2 %~ maintainState) compoundStateToText finalizer firstCompoundState
+  Terminal.showCursor
+
   if CMap.size finalState.fullSummary.failedBuilds == 0
     then exitSuccess
     else exitFailure
