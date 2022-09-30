392fa3e36d9e1218381214b66ae80bda8535c375
maralorn <mail@maralorn.de>
2022-09-30 04:43:25 +0200

    inline some functions

--- lib/NOM/Util.hs
+++ lib/NOM/Util.hs
@@ -9,35 +9,44 @@ foldMapEndo f = foldMap (f .> Endo) .> appEndo
 forMaybeM :: Monad m => [a] -> (a -> m (Maybe b)) -> m [b]
 forMaybeM = flip mapMaybeM
 
+-- Like in 'flow'
+{-# INLINE addPrintCache #-}
 addPrintCache :: Functor m => (update -> (istate, state) -> m (errors, (istate, Maybe state))) -> (state -> cache) -> update -> (istate, state, cache) -> m (errors, (istate, state, cache))
 addPrintCache updater cacher update (!oldIState, !oldState, oldCache) =
   updater update (oldIState, oldState) <|>> \(errors, (istate, stateMay)) ->
     let (!newState, newCache) = maybe (oldState, oldCache) (toSnd cacher) stateMay
      in (errors, (istate, newState, newCache))
 
+
+{-# INLINE (<||>) #-}
 (<||>) :: Applicative f => f Bool -> f Bool -> f Bool
 (<||>) = liftA2 (||)
 
 -- Like in  'errors'
+{-# INLINE hush #-}
 hush :: Either a b -> Maybe b
 hush = either (const Nothing) Just
 
 -- Like in 'flow'
+{-# INLINE (.>) #-}
 infixl 9 .>
 (.>) :: (a -> b) -> (b -> c) -> a -> c
 (.>) = flip (.)
 
 -- Like in 'flow'
+{-# INLINE (|>) #-}
 infixl 0 |>
 (|>) :: a -> (a -> b) -> b
 (|>) = (&)
 
 -- Functorial version of .>
+{-# INLINE (<.>>) #-}
 infixl 8 <.>>
 (<.>>) :: Functor f => (a -> f b) -> (b -> c) -> a -> f c
 f <.>> g = f .> fmap g
 
 -- Functorial version of |>
+{-# INLINE (<|>>) #-}
 infixl 8 <|>>
 (<|>>) :: Functor f => f a -> (a -> b) -> f b
 f <|>> g = f |> fmap g
