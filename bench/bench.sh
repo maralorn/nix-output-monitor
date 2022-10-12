6f3d05752b33a6273c9c4e6119d13de730111c22
maralorn <mail@maralorn.de>
2022-10-12 02:09:22 +0200

    Compress benchmark logs

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cabal run -- nom --json +RTS -s < bench/nom-shell-452e7d5.nix-log
+zstd -d < bench/nom-shell-452e7d5.nix-log.zst | cabal run -- nom --json +RTS -s
--- bench/slow-bench.sh
+++ bench/slow-bench.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pv --quiet --line-mode --rate-limit 20000 < bench/nom-shell-452e7d5.nix-log | cabal run -- nom --json +RTS -s
+zstd -d < bench/nom-shell-452e7d5.nix-log.zst | pv --quiet --line-mode --rate-limit 20000 | cabal run -- nom --json +RTS -s
