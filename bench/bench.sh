c3401fe6cbcd9e20d2f67e3e82e2f350cae0526c
maralorn <mail@maralorn.de>
2023-04-16 18:09:52 +0200

    Add eventlog to benchmark

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zstd -d < bench/nom-shell-452e7d5.nix-log.zst | cabal run -- nom --json +RTS -s
+zstd -d < bench/nom-shell-452e7d5.nix-log.zst | cabal run --ghc-option=-rtsopts --ghc-option=-eventlog -- nom --json +RTS -s -l -hT
