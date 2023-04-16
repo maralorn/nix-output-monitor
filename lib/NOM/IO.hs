3b46af944b180d7950df2a2dc821a404233b62e1
maralorn <mail@maralorn.de>
2023-04-16 20:00:37 +0200

    Fix a small memory leak

--- lib/NOM/IO.hs
+++ lib/NOM/IO.hs
@@ -217,7 +217,7 @@ processTextStream config parser updater maintenance printerMay finalize initialS
           |& Stream.tap (writeErrorsToBuffer bufferVar)
           |& Stream.mapMaybe rightToMaybe
           |& parser
-          |&. Stream.mapM_ (runUpdate bufferVar stateVar updater >=> atomically . modifyTVar bufferVar . flip (<>))
+          |&. Stream.mapM_ (runUpdate bufferVar stateVar updater >=> atomically . modifyTVar' bufferVar . flip (<>))
       waitForInput :: IO ()
       waitForInput = atomically $ check . not . ByteString.null =<< readTVar bufferVar
   printerMay & maybe keepProcessing \(printer, output_handle) -> do
