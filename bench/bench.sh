0d914af8a7be3c80adc20bc05ba17c1b9d622228
maralorn <mail@maralorn.de>
2023-06-11 16:24:15 +0200

    Change benchmarking flags

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zstd -d < bench/nom-shell-452e7d5.nix-log.zst | cabal run --ghc-options="-rtsopts -finfo-table-map -fdistinct-constructor-tables" -- nom --json +RTS -i0.01 -s -l -hi
+zstd -d < bench/nom-shell-452e7d5.nix-log.zst | cabal run -- nom --json
--- bench/profile.sh
+++ bench/profile.sh
@@ -6,8 +6,9 @@ mkdir -p "$FOLDER"
 git show --oneline -q > "$FOLDER/git-status"
 git status >> "$FOLDER/git-status"
 git diff >> "$FOLDER/git-status"
-zstd -d < bench/nom-shell-452e7d5.nix-log.zst | cabal run --enable-profiling --profiling-detail=none --ghc-option "-fprof-late" --ghc-option "-with-rtsopts=-pj -s$FOLDER/allocations" -- nom --json
-#mv nom.eventlog "$FOLDER/eventlog"
+zstd -d < bench/nom-shell-452e7d5.nix-log.zst | cabal run --enable-profiling --ghc-option "-rtsopts" -- nom --json +RTS -i0.01 -l -pj -hd -s$FOLDER/allocations
+mv nom.eventlog "$FOLDER/eventlog"
 mv nom.prof "$FOLDER/prof.json"
+eventlog2html "$FOLDER/eventlog"
 #hs-speedscope "$FOLDER/eventlog"
 echo See results in "$FOLDER/prof.json"
