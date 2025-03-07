5e02b6924db2733b9bca4d39e98849f7191b5228
maralorn <mail@maralorn.de>
2025-03-07 13:17:25 +0100

    Fix formatting

--- lib/NOM/Print.hs
+++ lib/NOM/Print.hs
@@ -248,11 +248,12 @@ stateToText config buildState@MkNOMV1State{..} = memo printWithSize . fmap Windo
   downloadsRunning = CMap.size runningDownloads
   uploadsRunning = CMap.size runningUploads
   uploadsDone = CMap.size completedUploads
-  finishedWithTraces = mconcat
-    [ markup yellow warning
-    , markup green " Finished "
-    , markup yellow ("with " <> show (length nixTraces) <> " traces reported by nix")
-    ]
+  finishedWithTraces =
+    mconcat
+      [ markup yellow warning
+      , markup green " Finished "
+      , markup yellow ("with " <> show (length nixTraces) <> " traces reported by nix")
+      ]
   finishMarkup
     | numFailedBuilds > 0 = markup red . ((warning <> " Exited after " <> show numFailedBuilds <> " build failures") <>)
     | not (null nixErrors) = markup red . ((warning <> " Exited with " <> show (length nixErrors) <> " errors reported by nix") <>)
