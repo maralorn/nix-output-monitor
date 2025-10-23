maralorn/nix-output-monitor

commit 611161fa47b2d01c88780142aeed654adad1b6f1

Strictify

lib/NOM/Update/Monad/CacheBuildReports.hs
@@ -6,23 +6,21 @@ module NOM.Update.Monad.CacheBuildReports (
   BuildReportMap,
 ) where
 
-import Relude
-
 import Control.Exception (IOException, catch)
 import Control.Monad.Writer.Strict (WriterT)
-import Data.Map.Strict qualified as Map
-import System.Directory (XdgDirectory (XdgCache), createDirectoryIfMissing, getXdgDirectory, removeFile)
-
 -- cassava
 import Data.Csv (FromRecord, HasHeader (NoHeader), ToRecord, decode, encode)
-
 -- data-default
 import Data.Default (def)
-
+import Data.Map.Strict qualified as Map
 -- filepath
-import System.FilePath ((</>))
 
 -- lock-file
+
+import NOM.Builds (Host (..))
+import Relude
+import System.Directory (XdgDirectory (XdgCache), createDirectoryIfMissing, getXdgDirectory, removeFile)
+import System.FilePath ((</>))
 import System.IO.LockFile (
   LockingException (CaughtIOException, UnableToAcquireLockFile),
   LockingParameters (retryToAcquireLock, sleepBetweenRetries),
@@ -30,8 +28,6 @@ import System.IO.LockFile (
   withLockExt,
   withLockFile,
  )
-
-import NOM.Builds (Host (..))
 
 -- Exposed functions
 
@@ -58,6 +54,7 @@ instance MonadCacheBuildReports IO where
 instance MonadCacheBuildReports m => MonadCacheBuildReports (StateT a m) where
   getCachedBuildReports = lift getCachedBuildReports
   updateBuildReports = lift . updateBuildReports
+
 instance (Monoid a, MonadCacheBuildReports m) => MonadCacheBuildReports (WriterT a m) where
   getCachedBuildReports = lift getCachedBuildReports
   updateBuildReports = lift . updateBuildReports
@@ -75,7 +72,7 @@ tryUpdateBuildReports updateFunc = do
 
 updateBuildReportsUnlocked :: (BuildReportMap -> BuildReportMap) -> FilePath -> IO BuildReportMap
 updateBuildReportsUnlocked updateFunc dir = do
-  !reports <- updateFunc <$> loadBuildReports dir
+  reports <- updateFunc <$> loadBuildReports dir
   reports <$ saveBuildReports dir reports
 
 memptyOnLockFail :: Monoid b => LockingException -> IO b

exe/Main.hs
@@ -4,6 +4,7 @@ import Control.Exception qualified as Exception
 import Control.Monad.Writer.Strict (WriterT (runWriterT))
 import Data.ByteString qualified as ByteString
 import Data.Hermes qualified as JSON
+import Data.Strict qualified as Strict
 import Data.Text.IO (hPutStrLn)
 import Data.Time (ZonedTime)
 import Data.Version (showVersion)
@@ -22,77 +23,77 @@ import NOM.State (NOMV1State (..), ProgressState (..), failedBuilds, fullSummary
 import NOM.State.CacheId.Map qualified as CMap
 import NOM.Update (detectLocalFinishedBuilds, maintainState, updateStateNixJSONMessage, updateStateNixOldStyleMessage)
 import NOM.Update.Monad (UpdateMonad)
-import Optics (Lens', gfield, (%), (%~), (.~), (^.), _2)
+import Optics (Lens', gfield, (%), (%~), (.~), (^.))
 import Optics qualified
 import Paths_nix_output_monitor (version)
 import Relude
-import Streamly.Internal.Data.Time.Units (AbsTime)
 import System.Console.ANSI qualified as Terminal
 import System.Console.Terminal.Size (Window)
 import System.Environment qualified as Environment
 import System.IO.Error qualified as IOError
 import System.Process.Typed qualified as Process
+import Type.Strict qualified as StrictType
 
-outputHandle ∷ Handle
+outputHandle :: Handle
 outputHandle = stderr
 
-defaultConfig ∷ Config
+defaultConfig :: Config
 defaultConfig =
   MkConfig
     { piping = False
     , silent = False
     }
 
-main ∷ IO Void
+main :: IO Void
 main = do
-  args ← Environment.getArgs
-  prog_name ← Environment.getProgName
+  args <- Environment.getArgs
+  prog_name <- Environment.getProgName
   case (args, prog_name) of
-    (["--version"], _) → do
+    (["--version"], _) -> do
       hPutStrLn stderr ("nix-output-monitor " <> fromString (showVersion version))
       exitWith =<< Process.runProcess (Process.proc "nix" ["--version"])
-    (nix_args, "nom-build") → do
+    (nix_args, "nom-build") -> do
       exitWith =<< runMonitoredCommand defaultConfig (Process.proc "nix-build" ("-v" : "--log-format" : "internal-json" : nix_args))
-    (nix_args, "nom-shell") → do
+    (nix_args, "nom-shell") -> do
       exitOnFailure =<< runMonitoredCommand defaultConfig{silent = True} (Process.proc "nix-shell" (("-v" : "--log-format" : "internal-json" : nix_args) <> ["--run", "exit"]))
       exitWith =<< Process.runProcess (Process.proc "nix-shell" nix_args)
-    ("build" : nix_args, _) → do
+    ("build" : nix_args, _) -> do
       exitWith =<< runMonitoredCommand defaultConfig (Process.proc "nix" ("build" : "-v" : "--log-format" : "internal-json" : nix_args))
-    ("shell" : nix_args, _) → do
+    ("shell" : nix_args, _) -> do
       exitOnFailure =<< runMonitoredCommand defaultConfig{silent = True} (Process.proc "nix" (("shell" : "-v" : "--log-format" : "internal-json" : nix_args) <> ["--command", "sh", "-c", "exit"]))
       exitWith =<< Process.runProcess (Process.proc "nix" ("shell" : nix_args))
-    ("develop" : nix_args, _) → do
+    ("develop" : nix_args, _) -> do
       exitOnFailure =<< runMonitoredCommand defaultConfig{silent = True} (Process.proc "nix" (("develop" : "-v" : "--log-format" : "internal-json" : nix_args) <> ["--command", "sh", "-c", "exit"]))
       exitWith =<< Process.runProcess (Process.proc "nix" ("develop" : nix_args))
-    ([], _) → do
-      finalState ← monitorHandle (Proxy @(Maybe NixOldStyleMessage, ByteString)) defaultConfig{piping = True} stdin
+    ([], _) -> do
+      finalState <- monitorHandle (Proxy @(Maybe NixOldStyleMessage, ByteString)) defaultConfig{piping = True} stdin
       if CMap.size finalState.fullSummary.failedBuilds + length finalState.nixErrors == 0
         then exitSuccess
         else exitFailure
-    (["--json"], _) → do
-      finalState ← monitorHandle (Proxy @(Either NOMError NixJSONMessage)) defaultConfig{piping = True} stdin
+    (["--json"], _) -> do
+      finalState <- monitorHandle (Proxy @(Either NOMError NixJSONMessage)) defaultConfig{piping = True} stdin
       if CMap.size finalState.fullSummary.failedBuilds + length finalState.nixErrors == 0
         then exitSuccess
         else exitFailure
-    xs → do
+    xs -> do
       hPutStrLn stderr helpText
       -- It's not a mistake if the user requests the help text, otherwise tell
       -- them off with a non-zero exit code.
       if any (liftA2 (||) (== "-h") (== "--help")) xs then exitSuccess else exitFailure
 
-exitOnFailure ∷ Process.ExitCode → IO ()
+exitOnFailure :: Process.ExitCode -> IO ()
 exitOnFailure = \case
-  code@Process.ExitFailure{} → exitWith code
-  _ → pass
+  code@Process.ExitFailure{} -> exitWith code
+  _ -> pass
 
-printIOException ∷ IOError.IOError → IO ()
+printIOException :: IOError.IOError -> IO ()
 printIOException io_exception = do
   let error_msg = case (IOError.isDoesNotExistError io_exception, IOError.ioeGetFileName io_exception) of
-        (True, Just cmd) → "Command '" <> toText cmd <> "' not available from $PATH."
-        _ → show io_exception
+        (True, Just cmd) -> "Command '" <> toText cmd <> "' not available from $PATH."
+        _ -> show io_exception
   hPutStrLn stderr $ markup red ("nix-output-monitor: " <> error_msg)
 
-runMonitoredCommand ∷ Config → Process.ProcessConfig () () () → IO Process.ExitCode
+runMonitoredCommand :: Config -> Process.ProcessConfig () () () -> IO Process.ExitCode
 runMonitoredCommand config process_config = do
   let process_config_with_handles =
         Process.setStdout Process.createPipe $
@@ -100,34 +101,34 @@ runMonitoredCommand config process_config = do
             Process.createPipe
             process_config
   Exception.handle ((ExitFailure 1 <$) . printIOException) $
-    Process.withProcessWait process_config_with_handles \process → do
+    Process.withProcessWait process_config_with_handles \process -> do
       void $ monitorHandle (Proxy @(Either NOMError NixJSONMessage)) config (Process.getStderr process)
-      exitCode ← Process.waitExitCode process
-      output ← ByteString.hGetContents (Process.getStdout process)
+      exitCode <- Process.waitExitCode process
+      output <- ByteString.hGetContents (Process.getStdout process)
       unless (ByteString.null output) $ ByteString.hPut stdout output
       pure exitCode
 
 data UpdateResult a = MkUpdateResult
-  { errors ∷ [NOMError]
-  , output ∷ ByteString
-  , newStateToPrint ∷ Maybe NOMV1State
-  , newState ∷ UpdaterState a
+  { errors :: [NOMError]
+  , output :: ByteString
+  , newStateToPrint :: Maybe NOMV1State
+  , newState :: UpdaterState a
   }
   deriving stock (Generic)
 
 data ProcessState a = MkProcessState
-  { updaterState ∷ UpdaterState a
-  , printFunction ∷ Maybe (Window Int) → (ZonedTime, AbsTime) → Text
+  { updaterState :: UpdaterState a
+  , printFunction :: Maybe (Window Int) -> (ZonedTime, Double) -> Text
   }
   deriving stock (Generic)
 
 class NOMInput a where
   type UpdaterState a
-  firstState ∷ NOMV1State → UpdaterState a
-  updateState ∷ UpdateMonad m ⇒ a → UpdaterState a → m (UpdateResult a)
-  nomState ∷ Lens' (UpdaterState a) NOMV1State
-  inputStream ∷ Handle → Stream (Either NOMError ByteString)
-  withParser ∷ (StreamParser a → IO t) → IO t
+  firstState :: NOMV1State -> UpdaterState a
+  updateState :: (StrictType.Strict (UpdaterState a), UpdateMonad m) => a -> UpdaterState a -> m (UpdateResult a)
+  nomState :: Lens' (UpdaterState a) NOMV1State
+  inputStream :: Handle -> Stream (Either NOMError ByteString)
+  withParser :: (StreamParser a -> IO t) -> IO t
 
 instance NOMInput (Either NOMError NixJSONMessage) where
   withParser body = JSON.withHermesEnv (body . parseStreamSimple . parseJSON)
@@ -146,32 +147,38 @@ instance NOMInput (Either NOMError NixJSONMessage) where
         , newState = fromMaybe old_state new_state
         }
 
+data OldStyleState = MkOldStyleState
+  { state :: NOMV1State
+  , lastRead :: Strict.Maybe Double
+  }
+  deriving stock (Generic)
+
 instance NOMInput (Maybe NixOldStyleMessage, ByteString) where
   withParser body = body (parseStreamAttoparsec parser)
-  type UpdaterState (Maybe NixOldStyleMessage, ByteString) = (Maybe AbsTime, NOMV1State)
+  type UpdaterState (Maybe NixOldStyleMessage, ByteString) = OldStyleState
   inputStream = readTextChunks
-  nomState = _2
-  firstState = (Nothing,)
+  nomState = gfield @"state"
+  firstState state' = MkOldStyleState{state = state', lastRead = Strict.Nothing}
   {-# INLINE updateState #-}
-  updateState input old_state = mkUpdateResult <$> updateStateNixOldStyleMessage input old_state
+  updateState input old_state = mkUpdateResult <$> updateStateNixOldStyleMessage input (Strict.toLazy old_state.lastRead, old_state.state)
    where
     mkUpdateResult ((errors, output), (new_timestamp, new_state)) =
       MkUpdateResult
         { errors
         , output
         , newStateToPrint = new_state
-        , newState = (new_timestamp, fromMaybe (snd old_state) new_state)
+        , newState = MkOldStyleState (fromMaybe (old_state.state) new_state) (Strict.toStrict new_timestamp)
         }
 
-monitorHandle ∷ ∀ a. NOMInput a ⇒ Proxy a → Config → Handle → IO NOMV1State
-monitorHandle _ config input_handle = withParser @a \streamParser → do
-  finalState ←
+monitorHandle :: forall a. (StrictType.Strict (UpdaterState a), NOMInput a) => Proxy a -> Config -> Handle -> IO NOMV1State
+monitorHandle _ config input_handle = withParser @a \streamParser -> do
+  finalState <-
     do
       Terminal.hHideCursor outputHandle
       hSetBuffering stdout (BlockBuffering (Just 1_000_000))
 
-      current_system ← Exception.handle ((Nothing <$) . printIOException) $ Just . decodeUtf8 <$> Process.readProcessStdout_ (Process.proc "nix" ["eval", "--extra-experimental-features", "nix-command", "--impure", "--raw", "--expr", "builtins.currentSystem"])
-      first_state ← initalStateFromBuildPlatform current_system
+      current_system <- Exception.handle ((Nothing <$) . printIOException) $ Just . decodeUtf8 <$> Process.readProcessStdout_ (Process.proc "nix" ["eval", "--extra-experimental-features", "nix-command", "--impure", "--raw", "--expr", "builtins.currentSystem"])
+      first_state <- initalStateFromBuildPlatform current_system
       let first_process_state = MkProcessState (firstState @a first_state) (stateToText config first_state)
       interact config streamParser (processStateUpdater @a config) (gfield @"updaterState" % nomState @a %~ maintainState) (.printFunction) (finalizer config) (inputStream @a input_handle) outputHandle first_process_state
       `Exception.finally` do
@@ -180,15 +187,15 @@ monitorHandle _ config input_handle = withParser @a \streamParser → do
   pure (finalState.updaterState ^. nomState @a)
 
 {-# INLINE processStateUpdater #-}
-processStateUpdater ∷
-  ∀ a m.
-  (NOMInput a, UpdateMonad m) ⇒
-  Config →
-  a →
+processStateUpdater ::
+  forall a m.
+  (NOMInput a, UpdateMonad m, StrictType.Strict (UpdaterState a)) =>
+  Config ->
+  a ->
   StateT (ProcessState a) m ([NOMError], ByteString)
 processStateUpdater config input = do
-  old_state ← get
-  updater_result ← updateState input (old_state.updaterState)
+  old_state <- get
+  updater_result <- updateState input (old_state.updaterState)
   put
     MkProcessState
       { updaterState = updater_result.newState
@@ -196,21 +203,21 @@ processStateUpdater config input = do
       }
   pure (updater_result.errors, updater_result.output)
 
-finalizer ∷
-  ∀ a m.
-  (NOMInput a, UpdateMonad m) ⇒
-  Config →
+finalizer ::
+  forall a m.
+  (NOMInput a, UpdateMonad m) =>
+  Config ->
   StateT (ProcessState a) m ()
 finalizer config = do
-  old_state ← get
-  newState ← (gfield @"progressState" .~ Finished) <$> execStateT (runWriterT detectLocalFinishedBuilds) (old_state.updaterState ^. nomState @a)
+  old_state <- get
+  newState <- (gfield @"progressState" .~ Finished) <$> execStateT (runWriterT detectLocalFinishedBuilds) (old_state.updaterState ^. nomState @a)
   put
     MkProcessState
       { updaterState = nomState @a .~ newState $ old_state.updaterState
       , printFunction = stateToText config newState
       }
 
-helpText ∷ Text
+helpText :: Text
 helpText =
   unlines
     [ "nix-output-monitor usages:"

fourmolu.yaml
@@ -1,3 +1,3 @@
 indentation: 2
-unicode: always
+unicode: never
 respectful: false

lib/Data/Sequence/Strict.hs
@@ -1,0 +1,17 @@
+module Data.Sequence.Strict (
+  Seq.sortOn,
+  Seq.filter,
+  (<|),
+  Data.Sequence.Strict.fromList,
+  Seq.null,
+  Seq ((Seq.:<|)),
+) where
+
+import Data.Sequence qualified as Seq
+import Relude
+
+(<|) :: a -> Seq a -> Seq a
+!item <| rest = item Seq.<| rest
+
+fromList :: [a] -> Seq.Seq a
+fromList = foldr (<|) mempty

lib/NOM/Builds.hs
@@ -1,11 +1,10 @@
 module NOM.Builds (parseHost, Derivation (..), StorePath (..), Host (..), FailType (..), parseStorePath, parseDerivation, storePathByteStringParser, derivationByteStringParser, parseIndentedStoreObject) where
 
-import Relude
-
 import Data.Attoparsec.ByteString qualified as Parser
 import Data.Attoparsec.ByteString.Char8 qualified as Parser.Char
 import Data.Attoparsec.Text qualified as TextParser
 import Data.Text qualified as Text
+import Relude
 
 data StorePath = StorePath
   { hash :: Text
@@ -85,7 +84,7 @@ instance ToText StorePath where
 instance ToString StorePath where
   toString = toString . toText
 
-data Host = Localhost | Host !Text
+data Host = Localhost | Host Text
   deriving stock (Ord, Eq, Show, Generic)
 
 instance ToText Host where

lib/NOM/Error.hs
@@ -1,8 +1,7 @@
 module NOM.Error (NOMError (..)) where
 
-import Relude
-
 import Control.Exception (IOException)
+import Relude
 
 data NOMError
   = InputError IOException

lib/NOM/IO.hs
@@ -15,7 +15,6 @@ import NOM.Print.Table as Table (bold, displayWidth, displayWidthBS, markup, red
 import NOM.Update.Monad (UpdateMonad, getNow)
 import Relude
 import Streamly.Data.Fold qualified as Fold
-import Streamly.Internal.Data.Time.Units (AbsTime)
 import Streamly.Prelude ((.:), (|&), (|&.))
 import Streamly.Prelude qualified as Stream
 import System.Console.ANSI (SGR (Reset), setSGRCode)
@@ -25,68 +24,68 @@ import System.IO qualified
 
 type Stream = Stream.SerialT IO
 
-type StreamParser update = Stream ByteString → Stream update
+type StreamParser update = Stream ByteString -> Stream update
 
 type Output = Text
 
-type UpdateFunc update state = ∀ m. UpdateMonad m ⇒ update → StateT state m ([NOMError], ByteString)
+type UpdateFunc update state = forall m. UpdateMonad m => update -> StateT state m ([NOMError], ByteString)
 
-type OutputFunc state = state → Maybe Window → (ZonedTime, AbsTime) → Output
+type OutputFunc state = state -> Maybe Window -> (ZonedTime, Double) -> Output
 
-type Finalizer state = ∀ m. UpdateMonad m ⇒ StateT state m ()
+type Finalizer state = forall m. UpdateMonad m => StateT state m ()
 
 type Window = Terminal.Size.Window Int
 
-readTextChunks ∷ Handle → Stream (Either NOMError ByteString)
+readTextChunks :: Handle -> Stream (Either NOMError ByteString)
 readTextChunks handle = loop
  where
   -- We read up-to 4kb of input at once. We will rarely need more than that for one successful parse (i.e. a line).
   -- I don‘t know much about computers, but 4k seems like something which would be cached efficiently.
-  bufferSize ∷ Int
+  bufferSize :: Int
   bufferSize = 4096
-  tryRead ∷ Stream (Either IOException ByteString)
+  tryRead :: Stream (Either IOException ByteString)
   tryRead = liftIO $ try $ ByteString.hGetSome handle bufferSize
-  loop ∷ Stream (Either NOMError ByteString)
+  loop :: Stream (Either NOMError ByteString)
   loop =
     tryRead >>= \case
-      Left err → Left (InputError err) .: loop -- Forward Exceptions, when we encounter them
-      Right "" → mempty -- EOF
-      Right input → Right input .: loop
+      Left err -> Left (InputError err) .: loop -- Forward Exceptions, when we encounter them
+      Right "" -> mempty -- EOF
+      Right input -> Right input .: loop
 
-runUpdate ∷
-  ∀ update state.
-  TVar ByteString →
-  TVar state →
-  UpdateFunc update state →
-  update →
+runUpdate ::
+  forall update state.
+  TVar ByteString ->
+  TVar state ->
+  UpdateFunc update state ->
+  update ->
   IO ByteString
 runUpdate bufferVar stateVar updater input = do
-  oldState ← readTVarIO stateVar
-  ((!errors, !output), !newState) ← runStateT (updater input) oldState
+  oldState <- readTVarIO stateVar
+  ((!errors, !output), !newState) <- runStateT (updater input) oldState
   atomically $ do
-    forM_ errors (\error' → modifyTVar bufferVar (appendError error'))
+    forM_ errors (\error' -> modifyTVar bufferVar (appendError error'))
     writeTVar stateVar newState
   pure output
 
-writeStateToScreen ∷ ∀ state. Bool → TVar Int → TVar state → TVar ByteString → (state → state) → OutputFunc state → Handle → IO ()
+writeStateToScreen :: forall state. Bool -> TVar Int -> TVar state -> TVar ByteString -> (state -> state) -> OutputFunc state -> Handle -> IO ()
 writeStateToScreen pad printed_lines_var nom_state_var nix_output_buffer_var maintenance printer output_handle = do
-  nowClock ← getZonedTime
-  now ← getNow
-  terminalSize ←
+  nowClock <- getZonedTime
+  now <- getNow
+  terminalSize <-
     Terminal.Size.hSize output_handle <&> \case
       -- We throw away non positive window sizes, which some terminals apparently report
       -- to avoid divisions by zero later on.
-      val@(Just window) | window.width > 0, window.height > 0 → val
-      _ → Nothing
+      val@(Just window) | window.width > 0, window.height > 0 -> val
+      _ -> Nothing
 
-  (nom_state, nix_output_raw) ← atomically do
+  (nom_state, nix_output_raw) <- atomically do
     -- ==== Time Critical Segment - calculating to much in atomically can lead
     -- to recalculations.  In this section we are racing with the input parsing
     -- thread to update the state.
     modifyTVar nom_state_var maintenance
     -- we bind those lazily to not calculate them during STM
-    ~nom_state ← readTVar nom_state_var
-    ~nix_output_raw ← swapTVar nix_output_buffer_var mempty
+    ~nom_state <- readTVar nom_state_var
+    ~nix_output_raw <- swapTVar nix_output_buffer_var mempty
     pure (nom_state, nix_output_raw)
   -- ====
 
@@ -105,18 +104,18 @@ writeStateToScreen pad printed_lines_var nom_state_var nix_output_buffer_var mai
       -- might be slightly to high in corner cases but that will only trigger
       -- sligthly more redraws which is totally acceptable.
       reflow_line_count_correction =
-        terminalSize <&> \size →
+        terminalSize <&> \size ->
           -- This division is fine, because we don‘t accept non positiv window sizes.
-          getSum $ foldMap (\line → Sum (displayWidthBS line `div` size.width)) nix_output
+          getSum $ foldMap (\line -> Sum (displayWidthBS line `div` size.width)) nix_output
 
-  (last_printed_line_count, lines_to_pad) ← atomically do
-    last_printed_line_count ← readTVar printed_lines_var
+  (last_printed_line_count, lines_to_pad) <- atomically do
+    last_printed_line_count <- readTVar printed_lines_var
     -- When the nom output suddenly gets smaller, it might jump up from the bottom of the screen.
     -- To prevent this we insert a few newlines before it.
     -- We only do this if we know the size of the terminal.
     let lines_to_pad = case reflow_line_count_correction of
-          Just reflow_correction | pad → max 0 (last_printed_line_count - reflow_correction - nix_output_length - nom_output_length)
-          _ → 0
+          Just reflow_correction | pad -> max 0 (last_printed_line_count - reflow_correction - nix_output_length - nom_output_length)
+          _ -> 0
         line_count_to_print = nom_output_length + lines_to_pad
     writeTVar printed_lines_var line_count_to_print
     pure (last_printed_line_count, lines_to_pad)
@@ -145,11 +144,11 @@ writeStateToScreen pad printed_lines_var nom_state_var nix_output_buffer_var mai
               )
             <>
             -- Insert the output to write to the screen.
-            ( output_to_print_with_newline_annotations & foldMap \(newline, line) →
+            ( output_to_print_with_newline_annotations & foldMap \(newline, line) ->
                 ( case newline of
-                    StayInLine → mempty
-                    MoveToNextLine → Builder.stringUtf8 (Terminal.cursorDownLineCode 1)
-                    PrintNewLine → Builder.byteString "\n"
+                    StayInLine -> mempty
+                    MoveToNextLine -> Builder.stringUtf8 (Terminal.cursorDownLineCode 1)
+                    PrintNewLine -> Builder.byteString "\n"
                 )
                   <> Builder.byteString line
             )
@@ -166,81 +165,81 @@ data ToNextLine = StayInLine | MoveToNextLine | PrintNewLine
 
 -- Depending on the current line of the output we are printing we need to decide
 -- how to move to a new line before printing.
-howToGoToNextLine ∷ Int → Maybe Int → Int → ToNextLine
+howToGoToNextLine :: Int -> Maybe Int -> Int -> ToNextLine
 howToGoToNextLine _ Nothing = \case
   -- When we have no info about terminal size, better be careful and always print
   -- newlines if necessary.
-  0 → StayInLine
-  _ → PrintNewLine
+  0 -> StayInLine
+  _ -> PrintNewLine
 howToGoToNextLine previousPrintedLines (Just correction) = \case
   -- When starting to print we are always in an empty line with the cursor at the start.
   -- So we don‘t need to go to a new line
-  0 → StayInLine
+  0 -> StayInLine
   -- When the current offset is smaller than the number of previously printed lines.
   -- e.g. we have printed 1 line, but before we had printed 2
   -- then we can probably move the cursor a row down without needing to print a newline.
   x
-    | x + correction < previousPrintedLines →
+    | x + correction < previousPrintedLines ->
         MoveToNextLine
   -- When we are at the bottom of the terminal we have no choice but need to
   -- print a newline and thus (sadly) flush the terminal
-  _ → PrintNewLine
+  _ -> PrintNewLine
 
-interact ∷
-  ∀ update state.
-  Config →
-  StreamParser update →
-  UpdateFunc update state →
-  (state → state) →
-  OutputFunc state →
-  Finalizer state →
-  Stream (Either NOMError ByteString) →
-  Handle →
-  state →
+interact ::
+  forall update state.
+  Config ->
+  StreamParser update ->
+  UpdateFunc update state ->
+  (state -> state) ->
+  OutputFunc state ->
+  Finalizer state ->
+  Stream (Either NOMError ByteString) ->
+  Handle ->
+  state ->
   IO state
 interact config parser updater maintenance printer finalize input_stream output_handle initialState =
   processTextStream config parser updater maintenance (Just (printer, output_handle)) finalize initialState input_stream
 
 -- frame durations are passed to threadDelay and thus are given in microseconds
 
-maxFrameDuration ∷ Int
+maxFrameDuration :: Int
 maxFrameDuration = 1_000_000 -- once per second to update timestamps
 
-minFrameDuration ∷ Int
+minFrameDuration :: Int
 minFrameDuration =
   -- this seems to be a nice compromise to reduce excessive
   -- flickering, since the movement is not continuous this low frequency doesn‘t
   -- feel to sluggish for the eye, for me.
   100_000 -- 10 times per second
 
-processTextStream ∷
-  ∀ update state.
-  Config →
-  StreamParser update →
-  UpdateFunc update state →
-  (state → state) →
-  Maybe (OutputFunc state, Handle) →
-  Finalizer state →
-  state →
-  Stream (Either NOMError ByteString) →
+processTextStream ::
+  forall update state.
+  Config ->
+  StreamParser update ->
+  UpdateFunc update state ->
+  (state -> state) ->
+  Maybe (OutputFunc state, Handle) ->
+  Finalizer state ->
+  state ->
+  Stream (Either NOMError ByteString) ->
   IO state
 processTextStream config parser updater maintenance printerMay finalize initialState inputStream = do
-  stateVar ← newTVarIO initialState
-  bufferVar ← newTVarIO mempty
-  let keepProcessing ∷ IO ()
+  stateVar <- newTVarIO initialState
+  bufferVar <- newTVarIO mempty
+  let keepProcessing :: IO ()
       keepProcessing =
         inputStream
           |& Stream.tap (writeErrorsToBuffer bufferVar)
           |& Stream.mapMaybe rightToMaybe
           |& parser
           |&. Stream.mapM_ (runUpdate bufferVar stateVar updater >=> atomically . modifyTVar bufferVar . flip (<>))
-      waitForInput ∷ IO ()
+      waitForInput :: IO ()
       waitForInput = atomically $ check . not . ByteString.null =<< readTVar bufferVar
-  printerMay & maybe keepProcessing \(printer, output_handle) → do
-    linesVar ← newTVarIO 0
-    let writeToScreen ∷ IO ()
+  printerMay & maybe keepProcessing \(printer, output_handle) -> do
+    linesVar <- newTVarIO 0
+    let writeToScreen :: IO ()
         writeToScreen = writeStateToScreen (not config.silent) linesVar stateVar bufferVar maintenance printer output_handle
-        keepPrinting ∷ IO ()
+        keepPrinting :: IO ()
         keepPrinting = forever do
           race_ (concurrently_ (threadDelay minFrameDuration) waitForInput) (threadDelay maxFrameDuration)
           writeToScreen
@@ -249,36 +248,36 @@ processTextStream config parser updater maintenance printerMay finalize initialS
     writeToScreen
   (if isNothing printerMay then (>>= execStateT finalize) else id) $ readTVarIO stateVar
 
-writeErrorsToBuffer ∷ TVar ByteString → Fold.Fold IO (Either NOMError ByteString) ()
+writeErrorsToBuffer :: TVar ByteString -> Fold.Fold IO (Either NOMError ByteString) ()
 writeErrorsToBuffer bufferVar = Fold.drainBy saveInput
  where
-  saveInput ∷ Either NOMError ByteString → IO ()
+  saveInput :: Either NOMError ByteString -> IO ()
   saveInput = \case
-    Left error' → atomically $ modifyTVar bufferVar (appendError error')
-    _ → pass
+    Left error' -> atomically $ modifyTVar bufferVar (appendError error')
+    _ -> pass
 
-appendError ∷ NOMError → ByteString → ByteString
+appendError :: NOMError -> ByteString -> ByteString
 appendError err prev = (if ByteString.null prev || ByteString.isSuffixOf "\n" prev then "" else "\n") <> nomError <> show err <> "\n"
 
-nomError ∷ ByteString
+nomError :: ByteString
 nomError = encodeUtf8 (markup (red . bold) "nix-output-monitor error: ")
 
-truncateOutput ∷ Maybe Window → Text → Text
+truncateOutput :: Maybe Window -> Text -> Text
 truncateOutput win output = maybe output go win
  where
-  go ∷ Window → Text
+  go :: Window -> Text
   go window = Text.intercalate "\n" $ truncateColumns window.width <$> truncateRows window.height
 
-  truncateColumns ∷ Int → Text → Text
+  truncateColumns :: Int -> Text -> Text
   truncateColumns columns line = if displayWidth line > columns then Table.truncate (columns - 1) line <> "…" <> toText (setSGRCode [Reset]) else line
 
-  truncateRows ∷ Int → [Text]
+  truncateRows :: Int -> [Text]
   truncateRows rows
     | length outputLines >= rows - outputLinesToAlwaysShow = take 1 outputLines <> [" ⋮ "] <> drop (length outputLines + outputLinesToAlwaysShow + 2 - rows) outputLines
     | otherwise = outputLines
 
-  outputLines ∷ [Text]
+  outputLines :: [Text]
   outputLines = Text.lines output
 
-outputLinesToAlwaysShow ∷ Int
+outputLinesToAlwaysShow :: Int
 outputLinesToAlwaysShow = 5

commit 618804578fc4de5ca2ecad1c72711c87c1bfcda1

Avoid doing lazy IO on build-reports.csv
This was broken in 611161f (Strictify, 2022-11-21).

Co-authored-by: Nathan van Doorn <nvd1234@gmail.com>

lib/NOM/Update/Monad/CacheBuildReports.hs
@@ -59,7 +59,7 @@ tryUpdateBuildReports updateFunc = do
 
 updateBuildReportsUnlocked :: (BuildReportMap -> BuildReportMap) -> FilePath -> IO BuildReportMap
 updateBuildReportsUnlocked updateFunc dir = do
-  reports <- updateFunc <$> loadBuildReports dir
+  !reports <- updateFunc <$> loadBuildReports dir
   reports <$ saveBuildReports dir reports
 
 buildReportsDir :: IO FilePath