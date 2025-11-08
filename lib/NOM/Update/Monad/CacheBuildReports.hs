b9577f43078e761362a42c76e58fa0478de3a075
maralorn <mail@maralorn.de>
2025-11-08 18:37:49 +0100

    Move toHost/fromHost

--- lib/NOM/Update/Monad/CacheBuildReports.hs
+++ lib/NOM/Update/Monad/CacheBuildReports.hs
@@ -49,6 +49,11 @@ instance FromNamedRecord BuildReport where
       <*> (parseTimeM True defaultTimeLocale timeFormat =<< m .: csvHeaderEndTime)
       <*> (m .: csvHeaderBuildSecs)
 
+toHost :: Text -> Host
+toHost = \case
+  "" -> Localhost
+  x -> Host x
+
 instance ToNamedRecord BuildReport where
   toNamedRecord m =
     namedRecord
@@ -57,6 +62,11 @@ instance ToNamedRecord BuildReport where
       , csvHeaderEndTime .= formatTime defaultTimeLocale timeFormat m.endTime
       , csvHeaderBuildSecs .= m.buildSecs
       ]
+
+fromHost :: Host -> Text
+fromHost = \case
+  Localhost -> ""
+  Host x -> x
 
 instance DefaultOrdered BuildReport where
   headerOrder _ =
@@ -130,15 +140,5 @@ toCSV =
     . traverse Map.assocs
     <=< Map.assocs
 
-fromHost :: Host -> Text
-fromHost = \case
-  Localhost -> ""
-  Host x -> x
-
 fromCSV :: [BuildReport] -> BuildReportMap
 fromCSV = Map.fromListWith Map.union . fmap (\BuildReport{..} -> ((host, drvName), Map.singleton endTime buildSecs))
-
-toHost :: Text -> Host
-toHost = \case
-  "" -> Localhost
-  x -> Host x
