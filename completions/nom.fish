38a86cc17aef295b275e082613bdddd1f2bd2be8
saygo-png <saygo.mail@proton.me>
2025-12-03 08:18:37 +0100

    fish completions

--- completions/nom.fish
+++ completions/nom.fish
@@ -1,7 +1,12 @@
+# This currently does nothing as fish does not have completions
+# for nix-* commands.
 complete --command nom-build --wraps nix-build
 complete --command nom-shell --wraps nix-shell
 
-complete --command nom --condition "__fish_seen_subcommand_from build" --wraps "nix build"
-complete --command nom --condition "__fish_seen_subcommand_from develop" --wraps "nix develop"
-complete --command nom --condition "__fish_seen_subcommand_from shell" --wraps "nix shell"
-complete --command nom --condition "__fish_seen_subcommand_from copy" --wraps "nix copy"
+function __fish_complete_nom_like_nix
+    # Remove the first word from the commandline because that is "nom"
+    set --local cmdline (commandline -xpc | string escape; commandline -ct)[2..-1]
+    complete --do-complete "nix $cmdline"
+end
+
+complete --command nom --condition "__fish_seen_subcommand_from build develop shell copy" --arguments '(__fish_complete_nom_like_nix)'
