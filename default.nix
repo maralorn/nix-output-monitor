6ea7e0ba9120443e84863cf119077cbed3d45f8f
maralorn <mail@maralorn.de>
2026-01-06 16:43:12 +0100

    Add doctests

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 { mkDerivation, ansi-terminal, async, attoparsec, base, bytestring
-, cassava, containers, directory, extra, filelock, filepath
-, fsnotify, hermes-json, HUnit, lib, nix-derivation, optics, random
-, relude, safe, safe-exceptions, stm, streamly-core, strict
-, terminal-size, text, time, transformers, typed-process, unix
-, word8
+, cassava, containers, directory, doctest-parallel, extra, filelock
+, filepath, fsnotify, hermes-json, HUnit, lib, nix-derivation
+, optics, random, relude, safe, safe-exceptions, stm, streamly-core
+, strict, terminal-size, text, time, transformers, typed-process
+, unix, word8
 }:
 mkDerivation {
   pname = "nix-output-monitor";
@@ -26,10 +26,10 @@ mkDerivation {
   ];
   testHaskellDepends = [
     ansi-terminal async attoparsec base bytestring cassava containers
-    directory extra filelock filepath fsnotify hermes-json HUnit
-    nix-derivation optics random relude safe safe-exceptions stm
-    streamly-core strict terminal-size text time transformers
-    typed-process word8
+    directory doctest-parallel extra filelock filepath fsnotify
+    hermes-json HUnit nix-derivation optics random relude safe
+    safe-exceptions stm streamly-core strict terminal-size text time
+    transformers typed-process word8
   ];
   homepage = "https://code.maralorn.de/maralorn/nix-output-monitor";
   description = "Processes output of Nix commands to show helpful and pretty information";
