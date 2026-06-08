58d5de04cc937947ebc5d25ee3f5013a23ca8e81
Léana 江 <leana.jiang+git@icloud.com>
2026-06-08 09:12:46 +0200

    drop pretty-simple from test deps

--- default.nix
+++ default.nix
@@ -1,9 +1,8 @@
 { mkDerivation, ansi-terminal, async, attoparsec, base, bytestring
 , cassava, containers, directory, extra, filelock, filepath
-, hermes-json, HUnit, lib, nix-derivation, optics, pretty-simple
-, random, relude, safe, safe-exceptions, stm, streamly-core, strict
-, terminal-size, text, time, transformers, typed-process, unix
-, word8
+, hermes-json, HUnit, lib, nix-derivation, optics, random, relude
+, safe, safe-exceptions, stm, streamly-core, strict, terminal-size
+, text, time, transformers, typed-process, unix, word8
 }:
 mkDerivation {
   pname = "nix-output-monitor";
@@ -26,9 +25,8 @@ mkDerivation {
   testHaskellDepends = [
     ansi-terminal async attoparsec base bytestring cassava containers
     directory extra filelock filepath hermes-json HUnit nix-derivation
-    optics pretty-simple random relude safe safe-exceptions stm
-    streamly-core strict terminal-size text time transformers
-    typed-process word8
+    optics random relude safe safe-exceptions stm streamly-core strict
+    terminal-size text time transformers typed-process word8
   ];
   homepage = "https://code.maralorn.de/maralorn/nix-output-monitor";
   description = "Processes output of Nix commands to show helpful and pretty information";
