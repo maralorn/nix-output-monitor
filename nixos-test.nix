887ff5d1314f484bd6c64d7b1b010c6f80f4e402
Léana 江 <leana.jiang+git@icloud.com>
2026-06-08 09:20:04 +0200

    document why busybox is in path

--- nixos-test.nix
+++ nixos-test.nix
@@ -16,6 +16,8 @@ pkgs.testers.runNixOSTest {
   nodes.machine =
     { pkgs, ... }:
     {
+      # "with_nix" tests builds with minimal path requirements (just busybox).
+      # Removing this will fail because qemu can't access internet.
       environment.systemPackages = [ pkgs.busybox ];
 
       nix.nixPath = [ "nixpkgs=${pin}" ];
@@ -39,16 +41,16 @@ pkgs.testers.runNixOSTest {
 
   testScript = /* python */ ''
     start_all()
+
+    # (with_nix) Ensure the nixpkgs to evaluate is available
     machine.succeed("nix-store -r ${pin}")
+    # (from files) Make sure golden-tests runtime and buildtime paths are available
+    # ${toString (all-golden-tests ++ map (x: x.drvPath) all-golden-tests)}
 
     # golden tests path are hard coded in the test binary.
     machine.execute("cp -r ${nom-test}/golden-tests .")
     machine.execute("mkdir -p test")
     machine.execute("cp -r ${./test/golden} test/golden")
-    exitcode, stdout = machine.execute("./golden-tests 2>&1 | tee stdout.log")
-    print(exitcode)
-
-    # ${toString (all-golden-tests ++ map (x: x.drvPath) all-golden-tests)}
-    # ${pin}
+    machine.succeed("./golden-tests 2>&1 | tee stdout.log")
   '';
 }
