cc2616e5e04564b59324bb49e40621d59aa17491
Léana 江 <leana.jiang+git@icloud.com>
2026-06-08 09:01:17 +0200

    restore haskell source

--- lib/NOM/IO/Input/OldStyle.hs
+++ lib/NOM/IO/Input/OldStyle.hs
@@ -34,7 +34,6 @@ data OldStyleState = MkOldStyleState
     -- output paths of currently running builds.
     lastRead :: Strict.Maybe Double
   }
-  deriving stock Show
 
 makeFieldLabelsNoPrefix ''OldStyleState
 
@@ -42,7 +41,6 @@ data OldStyleInput = MkOldStyleInput
   { parseResult :: Maybe NixOldStyleMessage
   , parsedInput :: ByteString
   }
-  deriving stock Show
 
 instance NOMInput OldStyleInput where
   withParser body = body (fmap (uncurry MkOldStyleInput . first join) <$> parseStreamAttoparsec parser)
